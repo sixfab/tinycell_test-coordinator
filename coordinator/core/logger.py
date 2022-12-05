2e724b32924fac8fe02b0822b64a75c7807b9b57
Gökhan Koçmarlı <gokhankocmarli@gmail.com>
2022-12-06 00:53:07 +0300

    feat: Add handler for console output

--- coordinator/core/logger.py
+++ coordinator/core/logger.py
@@ -12,10 +12,17 @@ if not os.path.exists(LOG_PATH):
 logger = logging.getLogger("tinycell_test-coordinator")
 logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter(LOG_FORMAT)
+
+# Handler for logging to file.
 log_handler = logging.handlers.TimedRotatingFileHandler(
     filename=f"{LOG_PATH}/coordinator-log", when="midnight", backupCount=6
 )
-
 log_handler.setFormatter(formatter)
 log_handler.set_name("log_handler")
 logger.addHandler(log_handler)
+
+# Handler for logging to console.
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+console_handler.set_name("console_handler")
+logger.addHandler(console_handler)
